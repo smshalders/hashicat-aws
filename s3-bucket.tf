15bd73a6eb0c7e230cbdaa7f0bc765a3f3b2f69c
smshalders <stevenshalders@gmail.com>
2021-10-07 13:39:18 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,10 +1,11 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket_prefix = prefix
+  bucket = "stevenshalderstest"
   acl    = "private"
-  
 
   versioning = {
     enabled = true
   }
+
+}
