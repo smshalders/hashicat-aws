e4e69cf99557a58deb55d5438dfc9e9e076ee45d
smshalders <stevenshalders@gmail.com>
2021-10-07 13:48:53 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,11 @@
 module "s3-bucket" {
   source  = "app.terraform.io/sshalders-deleteme-training/s3-bucket/aws"
   version = "2.2.0"
-  bucket_prefix = "stevenshalderstest123"
+  
+  bucket = "stevieg1963test"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
 }
